31409b7f6af03bc3a254709d8006ae831bf7327b
jimmyvo0512 <jimmyvo.05122002@gmail.com>
2022-10-20 22:53:45 +0700

    update: format SpamLord.py

--- python/SpamLord.py
+++ python/SpamLord.py
@@ -3,7 +3,7 @@ import os
 import re
 import pprint
 
-my_first_pat = '(\w+)@(\w+).edu'
+my_first_pat = "(\w+)@(\w+).edu"
 
 """ 
 TODO
@@ -26,32 +26,38 @@ NOTE: You shouldn't need to worry about this, but just so you know, the
 sure you check the StringIO interface if you do anything really tricky,
 though StringIO should support most everything.
 """
+
+
 def process_file(name, f):
     # note that debug info should be printed to stderr
     # sys.stderr.write('[process_file]\tprocessing file: %s\n' % (path))
     res = []
     for line in f:
-        matches = re.findall(my_first_pat,line)
+        matches = re.findall(my_first_pat, line)
         for m in matches:
-            email = '%s@%s.edu' % m
-            res.append((name,'e',email))
+            email = "%s@%s.edu" % m
+            res.append((name, "e", email))
     return res
+
 
 """
 You should not need to edit this function, nor should you alter
 its interface as it will be called directly by the submit script
 """
+
+
 def process_dir(data_path):
     # get candidates
     guess_list = []
     for fname in os.listdir(data_path):
-        if fname[0] == '.':
+        if fname[0] == ".":
             continue
-        path = os.path.join(data_path,fname)
-        f = open(path,'r')
+        path = os.path.join(data_path, fname)
+        f = open(path, "r")
         f_guesses = process_file(fname, f)
         guess_list.extend(f_guesses)
     return guess_list
+
 
 """
 You should not need to edit this function.
@@ -59,13 +65,16 @@ Given a path to a tsv file of gold e-mails and phone numbers
 this function returns a list of tuples of the canonical form:
 (filename, type, value)
 """
+
+
 def get_gold(gold_path):
     # get gold answers
     gold_list = []
-    f_gold = open(gold_path,'r')
+    f_gold = open(gold_path, "r")
     for line in f_gold:
-        gold_list.append(tuple(line.strip().split('\t')))
+        gold_list.append(tuple(line.strip().split("\t")))
     return gold_list
+
 
 """
 You should not need to edit this function.
@@ -74,6 +83,8 @@ computes the intersection and set differences, to compute the true
 positives, false positives and false negatives.  Importantly, it
 converts all of the values to lower case before comparing
 """
+
+
 def score(guess_list, gold_list):
     guess_list = [(fname, _type, value.lower()) for (fname, _type, value) in guess_list]
     gold_list = [(fname, _type, value.lower()) for (fname, _type, value) in gold_list]
@@ -85,35 +96,39 @@ def score(guess_list, gold_list):
     fn = gold_set - guess_set
 
     pp = pprint.PrettyPrinter()
-    #print 'Guesses (%d): ' % len(guess_set)
-    #pp.pprint(guess_set)
-    #print 'Gold (%d): ' % len(gold_set)
-    #pp.pprint(gold_set)
-    print 'True Positives (%d): ' % len(tp)
+    # print 'Guesses (%d): ' % len(guess_set)
+    # pp.pprint(guess_set)
+    # print 'Gold (%d): ' % len(gold_set)
+    # pp.pprint(gold_set)
+    print("True Positives (%d): " % len(tp))
     pp.pprint(tp)
-    print 'False Positives (%d): ' % len(fp)
+    print("False Positives (%d): " % len(fp))
     pp.pprint(fp)
-    print 'False Negatives (%d): ' % len(fn)
+    print("False Negatives (%d): " % len(fn))
     pp.pprint(fn)
-    print 'Summary: tp=%d, fp=%d, fn=%d' % (len(tp),len(fp),len(fn))
+    print("Summary: tp=%d, fp=%d, fn=%d" % (len(tp), len(fp), len(fn)))
+
 
 """
 You should not need to edit this function.
 It takes in the string path to the data directory and the
 gold file
 """
+
+
 def main(data_path, gold_path):
     guess_list = process_dir(data_path)
-    gold_list =  get_gold(gold_path)
+    gold_list = get_gold(gold_path)
     score(guess_list, gold_list)
+
 
 """
 commandline interface takes a directory name and gold file.
 It then processes each file within that directory and extracts any
 matching e-mails or phone numbers and compares them to the gold file
 """
-if __name__ == '__main__':
-    if (len(sys.argv) != 3):
-        print 'usage:\tSpamLord.py <data_dir> <gold_file>'
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("usage:\tSpamLord.py <data_dir> <gold_file>")
         sys.exit(0)
-    main(sys.argv[1],sys.argv[2])
+    main(sys.argv[1], sys.argv[2])
